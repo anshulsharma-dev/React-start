f98aef2f5b6a3806d430227d1450b5e06b8404d1
Your Name <sharmaanshul9796@gmail.com>
2023-10-16 04:19:28 +0530

    df

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,28 +1,101 @@
-import conf from "../conf/conf.js";
-import { Client, Account, ID } from "appwrite";  //from the documentation 
+// import conf from "../conf/conf.js";
+// import { Client, Account, ID } from "appwrite";  //from the documentation 
+
+// export class AuthService {
+//     client = new Client();
+//     account;
+
+//     constructor() {
+//         this.client
+//             .setEndpoint(conf.appwriteUrl)
+//             .setProject(conf.appwriteProjectId);
+//         this.account = new Account(this.client);
+//     }
+
+//     async createAccount({email, password, name}) {
+//         try {
+//             const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+//             if (userAccount) {
+//                 // call another method , so that if the userAccount exists make him login too
+//                 return this.login({email, password});
+//             } else {
+//                 return userAccount;
+//             }
+
+//         } catch (error) {
+//             throw error;
+//         }
+//     }
+
+//     async login({email, password}) {
+//         try {
+//            return await this.account.createEmailSession(email , password);
+//         } catch (error) {
+//             throw error;
+//         }
+//     }
+
+//     async getCurrentUser() {
+//     //     console.log("before try")
+//     //     try {
+//     //     console.log("after try")
+
+//     //        return await this.account.get();
+//     //     } catch (error) {
+//     //     console.log("Appwrite service :: getCurrentUser :: error", error);
+//     //     }
+//     //     return null;
+//     // }
+//     try {
+//         const CurrentUser = await this.account.get();
+//         if (CurrentUser) return CurrentUser
+//     } catch (error) {
+//         throw "You are not loggedin. Please login";
+//     }}
+
+//     async logout() {
+//         try {
+//             return await this.account.deleteSessions();
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error);
+//         }
+//     }
+// }
+
+
+// const authService = new AuthService();
+
+// export default authService;
+
+
+
+
+import conf from '../conf/conf.js';
+import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
     account;
 
-    constructor () {
+    constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
+            
     }
 
     async createAccount({email, password, name}) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
-
             if (userAccount) {
-                // call another method , so that if the userAccount exists make him login too
+                // call another method
                 return this.login({email, password});
             } else {
-                return userAccount;
+               return  userAccount;
             }
-
         } catch (error) {
             throw error;
         }
@@ -30,7 +103,7 @@ export class AuthService {
 
     async login({email, password}) {
         try {
-           return await this.account.createEmailSession(email , password);
+            return await this.account.createEmailSession(email, password);
         } catch (error) {
             throw error;
         }
@@ -38,23 +111,24 @@ export class AuthService {
 
     async getCurrentUser() {
         try {
-           return await this.account.get();
+            return await this.account.get();
         } catch (error) {
-        console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
     }
 
     async logout() {
+
         try {
-            return await this.account.deleteSessions();
+            await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
-
 const authService = new AuthService();
 
-export default authService;
+export default authService
--- 12MegaBlog/src/appwrite/config.js
+++ 12MegaBlog/src/appwrite/config.js
@@ -1,3 +1,145 @@
+// import conf from '../conf/conf.js';
+// import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+// export class Service{
+//     client = new Client();
+//     databases;
+//     bucket;
+
+//     constructor () {
+//         this.client
+//             .setEndpoint(conf.appwriteUrl)
+//             .setProject(conf.appwriteProjectId);
+//         this.databases = new Databases(this.client);
+//         this.bucket = new Storage(this.client);    
+
+//     }
+//     //   "slug" is also called as Document ID
+    
+//    // And also in this createDocument(,{}) it is a function taking parameters inside () and taking what needs to updated inside {}
+ 
+//     async createPost({title, slug, content, featuredImage, status, userId}) {
+//         try {
+//             return await this.databases.createDocument(
+//                conf.appwriteDatabaseId,
+//                conf.appwriteCollectionId,
+//                slug,
+//                {
+//                 title,
+//                 content,
+//                 status,
+//                 featuredImage,
+//                 userId
+//                }
+//             )    
+//         } catch (error) {
+//         console.log("Appwrite service :: getCurrentUser :: error", error);    
+//         }
+//     }
+
+//     async updatePost ( slug, {title, content, featuredImage, status}) {
+//         try {
+//             return await this.databases.updateDocument(
+//                 conf.appwriteDatabaseId,
+//                 conf.appwriteCollectionId,
+//                 slug,
+//                 {
+//                     status,
+//                     content,
+//                     title,
+//                     featuredImage
+//                 }
+//             )
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//         }
+//     }
+
+//     async deletePost (slug) {
+//         try {
+//             await this.databases.deleteDocument(
+//                 conf.appwriteCollectionId,
+//                 conf.appwriteDatabaseId,
+//                 slug
+//             )
+//             return true;
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//             return false;
+//         }
+//     }
+
+//     async getDocuments(slug){
+//         try {
+//             return await this.databases.getDocument(
+//                 conf.appwriteCollectionId,
+//                 conf.appwriteDatabaseId,
+//                 slug
+//             )    
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//             return false;
+//         }
+//     }
+
+//     // In listing all the documents by getPosts() func we can't do it simply because it will also list the posts which have  "inactive"  status, so we have to use "Queries" in this case and also, Queries can only be used if we have put indexing inside the database article.
+
+//     async getPosts ( queries = [Query.equal("staus" , "active")]) {
+//         try {
+//             return await this.databases.listDocuments(
+//                 conf.appwriteCollectionId,
+//                 conf.appwriteDatabaseId,
+//                 // [
+//                 //     Query.equal("staus" , "active")
+//                 // ]
+//                 queries
+//             )
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//             return false; 
+//         }
+//     }
+
+//     // file upload service
+//     async uploadFile(file){
+//         try {
+//             return await this.bucket.createFile(
+//                 conf.appwriteBucketId,
+//                 ID.unique(),
+//                 file
+//             )   
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//             return false; 
+//         }
+//     }
+
+//     async deleteFile(fileId){
+//         try {
+//             await this.bucket.deleteFile(
+//                 conf.appwriteBucketId,
+//                 fileId
+//             )
+//             return true;
+//         } catch (error) {
+//             console.log("Appwrite service :: getCurrentUser :: error", error); 
+//             return false;
+//         }
+//     }
+
+//     getFilePreview(fileId ){
+//         return this.bucket.getFilePreview(
+//             conf.appwriteBucketId,
+//             fileId
+//         )
+//     }
+// }
+
+// const service = new Service();
+// export default service
+
+
+
 import conf from '../conf/conf.js';
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
@@ -5,112 +147,109 @@ export class Service{
     client = new Client();
     databases;
     bucket;
-
-    constructor () {
-        this.client
-            .setEndpoint(conf.appwriteUrl)
-            .setProject(conf.appwriteProjectId);
-        this.databases = new Databases(this.client);
-        this.bucket = new Storage(this.client);    
-
-    }
-    //   "slug" is also called as Document ID
     
-   // And also in this createDocument(,{}) it is a function taking parameters inside () and taking what needs to updated inside {}
- 
-    async createPost({title, slug, content, featuredImage, status, userId}) {
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
         try {
             return await this.databases.createDocument(
-               conf.appwriteDatabaseId,
-               conf.appwriteCollectionId,
-               slug,
-               {
-                title,
-                content,
-                status,
-                featuredImage,
-                userId
-               }
-            )    
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
         } catch (error) {
-        console.log("Appwrite service :: getCurrentUser :: error", error);    
+            console.log("Appwrite serive :: createPost :: error", error);
         }
     }
 
-    async updatePost ( slug, {title, content, featuredImage, status}) {
+    async updatePost(slug, {title, content, featuredImage, status}){
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug,
                 {
-                    status,
-                    content,
                     title,
-                    featuredImage
+                    content,
+                    featuredImage,
+                    status,
+
                 }
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
+            console.log("Appwrite serive :: updatePost :: error", error);
         }
     }
 
-    async deletePost (slug) {
+    async deletePost(slug){
         try {
             await this.databases.deleteDocument(
-                conf.appwriteCollectionId,
                 conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
                 slug
+            
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
-            return false;
+            console.log("Appwrite serive :: deletePost :: error", error);
+            return false
         }
     }
 
-    async getDocuments(slug){
+    async getPost(slug){
         try {
             return await this.databases.getDocument(
-                conf.appwriteCollectionId,
                 conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
                 slug
-            )    
+            
+            )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
-            return false;
+            console.log("Appwrite serive :: getPost :: error", error);
+            return false
         }
     }
 
-    // In listing all the documents by getPosts() func we can't do it simply because it will also list the posts which have  "inactive"  status, so we have to use "Queries" in this case and also, Queries can only be used if we have put indexing inside the database article.
-
-    async getPosts ( queries = [Query.equal("staus" , "active")]) {
+    async getPosts(queries = [Query.equal("status", "active")]){
         try {
             return await this.databases.listDocuments(
-                conf.appwriteCollectionId,
                 conf.appwriteDatabaseId,
-                // [
-                //     Query.equal("staus" , "active")
-                // ]
-                queries
+                conf.appwriteCollectionId,
+                queries,
+                
+
             )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
-            return false; 
+            console.log("Appwrite serive :: getPosts :: error", error);
+            return false
         }
     }
 
     // file upload service
+
     async uploadFile(file){
         try {
             return await this.bucket.createFile(
                 conf.appwriteBucketId,
                 ID.unique(),
                 file
-            )   
+            )
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
-            return false; 
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
         }
     }
 
@@ -120,14 +259,14 @@ export class Service{
                 conf.appwriteBucketId,
                 fileId
             )
-            return true;
+            return true
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error); 
-            return false;
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
         }
     }
 
-    getFilePreview(fileId ){
+    getFilePreview(fileId){
         return this.bucket.getFilePreview(
             conf.appwriteBucketId,
             fileId
@@ -135,5 +274,6 @@ export class Service{
     }
 }
 
-const service = new Service();
+
+const service = new Service()
 export default service
